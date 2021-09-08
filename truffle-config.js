6c63dcb9fe1e988e706ef8df5c7bd6dbb914714f
NamiChen <1525876629@qq.com>
2021-09-08 22:24:17 +0800

    test truffle

--- truffle-config.js
+++ truffle-config.js
@@ -5,16 +5,12 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-
-  plugins: [
-    "truffle-plugin-hello"
-  ],
-  networks: {
-   development: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   },}
+  //networks: {
+  //  development: {
+  //    host: "127.0.0.1",
+  //    port: 7545,
+  //    network_id: "*"
+  //  },
   //  test: {
   //    host: "127.0.0.1",
   //    port: 7545,
